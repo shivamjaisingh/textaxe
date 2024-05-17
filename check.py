4d5ebf6dd4b05fecb2e0ef49637ca63d171f53ae
Shivam Jai Singh <20377780+shivamjaisingh@users.noreply.github.com>
2024-05-17 06:53:36 -0400

    Update check.py

--- check.py
+++ check.py
@@ -1,7 +1,7 @@
 import sys
 import json
 from aylienapiclient import textapi
-client = textapi.Client("7322ec29", "6b8f2a3e7ad63c90b50fdc435367e4ac")
+client = textapi.Client("xxxxec29", "xxxxxxxxxxxxxxxx0b50fdc435367e4ac")
 #print sys.argv[1]
 sentiment = client.Sentiment({'text': sys.argv[1]})
 #sentiment = client.Sentiment({'text': 'I hate you, but I love you'})
